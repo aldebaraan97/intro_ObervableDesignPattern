29f7a074d6b291d3fc11861b1cb6983d83ca13b0
Aldebaraan <aldecanedo97@gmail.com>
2024-11-08 13:03:17 -0500

    Second commit

---
+++ src/main/java/org/example/observablepropertydemo/ResizableCircleRectangle.java
@@ -0,0 +1,43 @@
+package org.example.observablepropertydemo;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import java.util.Stack;
+
+public class ResizableCircleRectangle extends Application {
+    // Create a circle and a rectangle
+    private Circle circle = new Circle(60);
+    private Rectangle rectangle = new Rectangle(120,120);
+
+    // Place clock and label in border pane
+    private StackPane pane = new StackPane();
+
+    @Override // Override the staet method in the Application class
+    public void start(Stage primaryStage) {
+        circle.setFill(Color.GRAY);
+        rectangle.setFill(Color.WHITE);
+        rectangle.setStroke(Color.BLACK);
+        pane.getChildren().addAll(rectangle, circle);
+
+        // Create a scene and place the pane in the stage
+        Scene scene = new Scene(pane, 140, 140);
+        primaryStage.setTitle("ResizableCircleRectangle");
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+
+        pane.widthProperty().addListener(ov -> resize());
+        pane.heightProperty().addListener(ov -> resize());
+    }
+
+    private void resize() {
+        double length = Math.min(pane.getWidth(), pane.getHeight());
+        circle.setRadius(length / 2 - 15);
+        rectangle.setWidth(length - 30);
+        rectangle.setHeight(length - 30);
+    }
+}
